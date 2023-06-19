774293bea01381e556a10517607df12c15153c86
pàshú chy <chypashu@gmail.com>
2023-06-19 11:11:58 +0545

    maked play btn functional

--- js/script.js
+++ js/script.js
@@ -18,9 +18,51 @@ function computer_choice(hands) {
   let computer_choice = bot_choice[random_number];
   let computer_choice_image = document.getElementById("computer_choice");
   computer_choice_image.src = handObj[computer_choice];
-  refree(hands, computer_choice)
+  refree(hands, computer_choice);
 }
 
 function refree(hands, computer_choice) {
-  
+  if (hands === "rock" && computer_choice === "paper") {
+    setDecision("Computer");
+  }
+  if (hands === "rock" && computer_choice === "scissors") {
+    setDecision("human");
+  }
+  if (hands === "rock" && computer_choice === "rock") {
+    setDecision("Draw");
+  }
+  if (hands === "paper" && computer_choice === "paper") {
+    setDecision("Draw");
+  }
+  if (hands === "paper" && computer_choice === "scissors") {
+    setDecision("Computer");
+  }
+  if (hands === "paper" && computer_choice === "rock") {
+    setDecision("human");
+  }
+  if (hands === "scissors" && computer_choice === "rock") {
+    setDecision("Computer");
+  }
+  if (hands === "scissors" && computer_choice === "paper") {
+    setDecision("human");
+  }
+  if (hands === "scissors" && computer_choice === "scissors") {
+    setDecision("Draw");
+  }
+}
+
+function setDecision(ref_decesion) {
+  let decesion = document.getElementById("decesion");
+  decesion.innerHTML = ref_decesion;
+  let result_board = document.getElementById("result_board");
+  result_board.style.display = "flex";
+  let hand = document.getElementById("hand");
+  hand.style.display = "none";
+}
+
+function reset() {
+  let result_board = document.getElementById("result_board");
+  result_board.style.display = "none";
+  let hand = document.getElementById("hand");
+  hand.style.display = "flex";
 }
